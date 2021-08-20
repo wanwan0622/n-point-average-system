60e6c7cae6406357708a1a2bf70519449abd4074
wanwan0622 <minnie20000622@yahoo.co.jp>
2021-08-20 19:10:20 +0900

    update

--- n_point_average_system.py
+++ n_point_average_system.py
@@ -2,6 +2,8 @@ import wave
 import matplotlib.pyplot as plt
 from scipy import signal as sg
 import numpy as np
+import playsound
+from scipy.io.wavfile import write
 
 def n_point_average_system(wavedata, Fs, n):
     # wavedata:音声データ, Fs:サンプリング周波数, n:n点平均のn
@@ -45,10 +47,63 @@ if __name__ == "__main__":
     time = np.arange(0, len(wavedata)) / Fs
     plt.figure(figsize=(16, 5))
     plt.plot(time, wavedata)
-    plt.title("Wave data of original music", fontsize=12)
+    plt.title("Wave data of the original sound", fontsize=12)
     plt.xlabel("Time [sec]")
     plt.ylabel("Amplitude")
     plt.xlim(0, 5)
     plt.show()
 
-    
+    # 短時間フーリエ変換
+    # 窓関数はハニング窓、窓長は20msec、窓の重なりは10msec
+    F, T, Adft = sg.stft(wavedata, fs=Fs, window="hann", nperseg=0.02*Fs, noverlap=0.01*Fs)
+
+    # 振幅値をdBに変換
+    P = 10 * np.log(np.abs(Adft))
+
+    # 短時間フーリエ変換のスペクトログラムを表示
+    plt.figure(figsize=(16, 5))
+    plt.title("Spectrogram of the original sound", fontsize=12)
+    plt.xlabel("Time [sec]")
+    plt.ylabel("Frequency [kHz]")
+    plt.xlim(0, 5)
+    plt.pcolormesh(T, F/1000, P, cmap="rainbow")
+    plt.colorbar(orientation="horizontal").set_label("Amplitude [dB]")
+    plt.show()
+
+    # 加工前の音を再生
+    playsound.playsound(fname)
+
+    # 3点平均システムをかけたサウンドを生成
+    three_point_average_wavedata = n_point_average_system(wavedata, Fs, 3)
+
+    # 加工後の音の波形グラフを表示
+    plt.figure(figsize=(16, 5))
+    plt.plot(time, three_point_average_wavedata)
+    plt.title("Wave data of the three point average sound", fontsize=12)
+    plt.xlabel("Time [sec]")
+    plt.ylabel("Amplitude")
+    plt.xlim(0, 5)
+    plt.show()
+
+    # 短時間フーリエ変換
+    # 窓関数はハニング窓、窓長は20msec、窓の重なりは10msec
+    F2, T2, Adft2 = sg.stft(three_point_average_wavedata, fs=Fs, window="hann", nperseg=0.02*Fs, noverlap=0.01*Fs)
+
+    # 振幅値をdBに変換
+    P2 = 10 * np.log(np.abs(Adft2))
+
+    # 短時間フーリエ変換のスペクトログラムを表示
+    plt.figure(figsize=(16, 5))
+    plt.title("Spectrogram of the three point average sound", fontsize=12)
+    plt.xlabel("Time [sec]")
+    plt.ylabel("Frequency [kHz]")
+    plt.xlim(0, 5)
+    plt.pcolormesh(T2, F2/1000, P2, cmap="rainbow")
+    plt.colorbar(orientation="horizontal").set_label("Amplitude [dB]")
+    plt.show()
+
+    # 加工後の音を書き出し
+    # write('three_point_aberage.wav', Fs, three_point_average_wavedata)
+
+    # 加工後の音を再生
+    playsound.playsound('./three_point_aberage.wav')
